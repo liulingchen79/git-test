caa567ae5486e9a35d496282960863f940b3c253
lingchen <lingchen79@163.com>
2021-06-08 15:54:42 +0800

    first commit

---
+++ src/main/java/com/lingchen/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.lingchen.git;
+
+/**
+ * @description
+ * @auther LiuLingchen
+ * @create 2021-06-08 15:50
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
